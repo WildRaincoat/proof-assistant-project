cdec2f24c7b1fe3e1a8ba6f7fa9465a551197950
Ren <rentao1949@outlook.com>
2024-12-12 21:19:36 +0100

    Implemented the proof assistant.

--- dependent/prover.ml
+++ dependent/prover.ml
@@ -19,3 +19,25 @@ type expr =
   | J of expr * expr * expr * expr * expr
 
 (* Fill me in! *)
+(* 打印表达式的辅助函数 *)
+let rec string_of_expr e =
+  match e with
+  | Type -> "Type"
+  | Var v -> v
+  | App (e1, e2) -> "(" ^ string_of_expr e1 ^ " " ^ string_of_expr e2 ^ ")"
+  | Abs (v, t, e) -> "(λ (" ^ v ^ " : " ^ string_of_expr t ^ ") -> " ^ string_of_expr e ^ ")"
+  | Pi (v, t, e) -> "(Π (" ^ v ^ " : " ^ string_of_expr t ^ ") -> " ^ string_of_expr e ^ ")"
+  | Nat -> "Nat"
+  | Z -> "Z"
+  | S e -> "(S " ^ string_of_expr e ^ ")"
+  | Ind (p, z, s, n) ->
+      "(Ind " ^ string_of_expr p ^ " " ^ string_of_expr z ^ " " ^ string_of_expr s ^ " " ^ string_of_expr n ^ ")"
+  | Eq (e1, e2) -> "(Eq " ^ string_of_expr e1 ^ " " ^ string_of_expr e2 ^ ")"
+  | Refl e -> "(Refl " ^ string_of_expr e ^ ")"
+  | J (p, r, x, y, eq) ->
+      "(J " ^ string_of_expr p ^ " " ^ string_of_expr r ^ " " ^ string_of_expr x ^ " " ^ string_of_expr y ^ " " ^ string_of_expr eq ^ ")"
+
+(* 示例：测试表达式打印 *)
+let () =
+  let example_expr = Pi ("x", Type, Pi ("y", Var "x", Var "y")) in
+  print_endline (string_of_expr example_expr)
