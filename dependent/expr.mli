86cf9f5b7c81a11f514f93b69a5dec5e79207f4a
Ren <rentao1949@outlook.com>
2024-12-13 14:52:09 +0100

    Implemented the proof assistant.

---
+++ dependent/expr.mli
@@ -0,0 +1,21 @@
+type var = string
+
+(** Expressions. *)
+type expr =
+  | Type
+  | Var of var
+  | App of expr * expr
+  | Abs of var * expr * expr
+  | Pi of var * expr * expr
+  | Nat
+  | Z
+  | S of expr
+  | Ind of expr * expr * expr * expr 
+  | Eq of expr * expr
+  | Refl of expr
+  | J of expr * expr * expr * expr * expr
+
+(*5.5*)
+type context = (var * (expr * expr option)) list
+(*5.9*)
+exception Type_error of string
